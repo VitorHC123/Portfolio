6ecfa285c24110c2db7b3414534b0c1813668ddc
Vitor Correia <vitinho.correia321@outlook.com>
2025-09-12 14:57:24 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  const content = document.getElementById("content");
+
+  preloader.classList.add("fade-out");
+
+  setTimeout(() => {
+    preloader.style.display = "none";
+    content.style.display = "block";
+  }, 1000);
+});
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 0.1, 
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target); 
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
